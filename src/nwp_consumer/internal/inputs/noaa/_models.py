d91094e689b575355eb10c6f43afd15f1d1eee21
Jacob Bieker <jacob@bieker.tech>
2023-12-19 14:06:12 +0000

    Address more comments

--- src/nwp_consumer/internal/inputs/noaa/_models.py
+++ src/nwp_consumer/internal/inputs/noaa/_models.py
@@ -8,8 +8,6 @@ class NOAAFileInfo(internal.FileInfoModel):
         self, it: dt.datetime, filename: str, currentURL: str, step: int,
     ) -> "NOAAFileInfo":
         self._it = it
-        # The name of the file when stored by the storer. We decompress from bz2
-        # at download time, so we don't want that extension on the filename.
         self._filename = filename
         self._url = currentURL
         self.step = step
@@ -18,8 +16,6 @@ class NOAAFileInfo(internal.FileInfoModel):
         return self._filename
 
     def filepath(self) -> str:
-        # The filename in the fully-qualified filepath still has the .bz2 extension
-        # so add it back in
         return self._url + "/" + self._filename
 
     def it(self) -> dt.datetime:
