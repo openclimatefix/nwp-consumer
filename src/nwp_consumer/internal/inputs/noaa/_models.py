86a77dd05ff4769aee987eaaa7d82898c642bc06
devsjc <47188100+devsjc@users.noreply.github.com>
2024-01-30 16:04:56 +0000

    Ensure only available parameters are pulled from ECMWF (#99)
    * Ensure only available parameters are pulled from ECMWF
    
    * Update unittests for ecmwf client

--- src/nwp_consumer/internal/inputs/noaa/_models.py
+++ src/nwp_consumer/internal/inputs/noaa/_models.py
@@ -5,18 +5,33 @@ from nwp_consumer import internal
 
 class NOAAFileInfo(internal.FileInfoModel):
     def __init__(
-        self, it: dt.datetime, filename: str, currentURL: str, step: int,
-    ) -> "NOAAFileInfo":
+        self,
+        it: dt.datetime,
+        filename: str,
+        currentURL: str,
+        step: int,
+    ) -> None:
         self._it = it
         self._filename = filename
         self._url = currentURL
         self.step = step
 
     def filename(self) -> str:
+        """Overrides the corresponding method in the parent class."""
         return self._filename
 
     def filepath(self) -> str:
+        """Overrides the corresponding method in the parent class."""
         return self._url + "/" + self._filename
 
     def it(self) -> dt.datetime:
+        """Overrides the corresponding method in the parent class."""
         return self._it
+
+    def steps(self) -> list[int]:
+        """Overrides the corresponding method in the parent class."""
+        return [self.step]
+
+    def variables(self) -> list[str]:
+        """Overrides the corresponding method in the parent class."""
+        raise NotImplementedError()
