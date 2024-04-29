e74fca3ac87c16db00ba473eca4911ab90ad2658
devsjc <47188100+devsjc@users.noreply.github.com>
2024-04-29 10:42:34 +0100

    feat(service): Create consumer service

---
+++ src/nwp_consumer/internal/core/service/consumer.py
@@ -0,0 +1,8 @@
+from .. import (domain, ports)
+
+
+class NWPArchivalService(ports.NWPConsumerService):
+    """Service for archiving NWP data implementing the NWPConsumerService interface.
+
+    This service consumes NWP data and writes it to an archival store
+    """
