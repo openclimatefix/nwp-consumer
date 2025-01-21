fac7a794f720cc5155ea1fabce0fb0a3576eafd4
peterdudfield <peter.dudfield@hotmail.com>
2025-01-21 18:46:19 +0000

    add test

--- src/nwp_consumer/internal/repositories/raw_repositories/test_mo_datahub.py
+++ src/nwp_consumer/internal/repositories/raw_repositories/test_mo_datahub.py
@@ -3,6 +3,7 @@ import datetime as dt
 import os
 import pathlib
 import unittest
+from unittest.mock import patch
 
 from returns.result import Failure, ResultE, Success
 
@@ -89,3 +90,43 @@ class TestMetOfficeDatahubRawRepository(unittest.TestCase):
                 else:
                     self.assertIsInstance(region_result, Success, msg=f"{region_result}")
 
+        @patch.dict(os.environ, {"MODEL": "UKV"}, clear=True)
+        def test_convert_ukv(self) -> None:
+
+            @dataclasses.dataclass
+            class TestCase:
+                filename: str
+                expected_coords: NWPDimensionCoordinateMap
+                should_error: bool
+
+            tests: list[TestCase] = [
+                TestCase(
+                    filename="test_MO_UKV_agl_relative-humidity_1.5_2025012112.grib",
+                    expected_coords=dataclasses.replace(
+                        MetOfficeDatahubRawRepository.model().expected_coordinates,
+                        init_time=[dt.datetime(2025, 1, 21, 12, tzinfo=dt.UTC)],
+                        variable=[Parameter.TEMPERATURE_SL],
+                        step=[0],
+                    ),
+                    should_error=False,
+                ),
+            ]
+
+            for t in tests:
+                with self.subTest(name=t.filename):
+                    # Attempt to convert the file
+                    result = MetOfficeDatahubRawRepository._convert(
+                        path=pathlib.Path(__file__).parent.absolute() / "test_gribs" / t.filename,
+                    )
+                    region_result: ResultE[dict[str, slice]] = result.do(
+                        region
+                        for das in result
+                        for da in das
+                        for region in NWPDimensionCoordinateMap.from_xarray(da).bind(
+                            t.expected_coords.determine_region,
+                        )
+                    )
+                    if t.should_error:
+                        self.assertIsInstance(region_result, Failure, msg=f"{region_result}")
+                    else:
+                        self.assertIsInstance(region_result, Success, msg=f"{region_result}")
